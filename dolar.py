c19137527029c49ed74956f686d0ff4300f1c5ec
Sena <pedrosena.hgp@gmail.com>
2026-05-30 14:12:13 -0300

    Add files via upload

---
+++ dolar.py
@@ -0,0 +1,12 @@
+input("Digite o valor em dolar: ")
+dolar = 5.21
+real_dolar = print(f"O valor em dolar é: {dolar} reais" )
+real = float(input("Digite o valor em real: "))
+
+dolar_real = real / dolar
+dolar_real = round(dolar_real, 2)
+
+
+print(f"O valor em real é: {dolar_real} dolar")
+
+
